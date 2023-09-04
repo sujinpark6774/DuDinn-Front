b672b8dfd70489092539a0e46591734dba9e9b01
sujin <cmadmin@kt.com>
2023-09-04 17:46:59 +0900

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <FileList />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import FileList from './pages/FileList.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    FileList
   }
 }
 </script>
---
+++ src/pages/FileList.vue
@@ -0,0 +1,25 @@
+<template>
+  {{ fileData }}
+</template>
+  
+<script>
+export default {
+  name: 'FileList',
+  components: {
+  },
+  data() {
+    return {
+        fileData: [],
+    }
+  },
+  methods: {
+    async fileListGet() {
+      const res = await this.$axios.get('/api/file/select/files')
+      this.fileData = res.data
+    }
+  },
+  mounted() {
+    this.fileListGet();
+  }
+}
+</script>
