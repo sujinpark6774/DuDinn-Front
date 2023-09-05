92ad59ea6ebf1c1c30dded6369511920832e8860
sujin <cmadmin@kt.com>
2023-09-05 17:50:04 +0900

    jqxGrid 추가

---
+++ src/components/JqxGrid.vue
@@ -0,0 +1,17 @@
+<template>
+    <JqxGrid />
+</template>
+
+<script>
+import JqxGrid from "jqwidgets-scripts/jqwidgets-vue/vue_jqxgrid.vue";
+
+export default {
+    name: 'App',
+    components: {
+        JqxGrid
+    }
+}
+</script>
+
+<style></style>
+  
